563c531aaddfea411968c80abcbc9e4d60d496be
Nadji_Tan <nadjitan@yahoo.com.ph>
2021-01-18 17:28:30 +0800

    Temp reset styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport", content="width=device-width, initial-scale=1.0">
   <title>Agila - Login</title>
 
+  <link rel="icon" href="{{ asset('img/Symbol.svg') }}"/>
   <link rel="stylesheet" type="text/css" href="css/app.css" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport", content="width=device-width, initial-scale=1.0">
   <title>Agila @yield('pageTitle')</title>
+  
   <link rel="icon" href="{{ asset('img/Symbol.svg') }}"/>
-
   <link rel="stylesheet" type="text/css" href="css/app.css" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
