f5d42dfaddede83f90e09a849418498cdc96c627
Nadji_Tan <nadjitan@yahoo.com.ph>
2021-01-18 16:30:08 +0800

    styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Agila - Login</title>
 
-  <link rel="stylesheet" type="text/css" href="css/app.css" />
+  <link rel="stylesheet" type="text/css" href="../css/app.css" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
     href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
